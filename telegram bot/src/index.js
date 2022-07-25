70d966a88d5a094925a6e197786cffab10ee2017
avazbekdeveloper11 <avazbekdeveloper11@gmail.com>
2022-07-25 00:25:26 -0700

    Telegram bot shablon

---
+++ telegram bot/src/index.js
@@ -0,0 +1,10 @@
+const telegramBot = new require("node-telegram-bot-api")
+
+require('dotenv').config();
+
+const TOKEN = process.env.TOKEN;
+
+const bot = new telegramBot(TOKEN, { polling: true });
+
+bot.on('message', (message) => {}
+);
